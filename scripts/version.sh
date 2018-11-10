8fcb2b90a6b7804baf555ea47d5098c927dd62c9
Martijn Dwars <martijn.dwars@oracle.com>
2018-11-10 18:53:49 +0100

    Add release scripts and instructions

---
+++ scripts/version.sh
@@ -0,0 +1,20 @@
+#/usr/bin/env sh
+set -eu
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: version.sh OLD_VERSION NEW_VERSION" && exit 1
+fi
+
+OLD_VERSION=$1
+NEW_VERSION=$2
+
+files=(
+  "build.gradle"
+  "README.md"
+)
+
+for file in ${files[@]}; do
+  sed -i '' "s/$OLD_VERSION/$NEW_VERSION/g" $file
+done
+
+
